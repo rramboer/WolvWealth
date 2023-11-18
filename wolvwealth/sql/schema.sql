f0617e943e9b91d8dce4d6dcf1fb21d6e550e3fe
cvano <cawv2003@gmail.com>
2023-11-18 18:48:04 -0500

    Add SQLite schema.

--- wolvwealth/sql/schema.sql
+++ wolvwealth/sql/schema.sql
@@ -11,12 +11,19 @@ CREATE TABLE users (
 );
 
 CREATE TABLE optimizations (
+  optimize_id INT NOT NULL AUTO_INCREMENT
   username VARCHAR(20) NOT NULL,
-  opt_id INT NOT NULL AUTO_INCREMENT
-  email VARCHAR(40) NOT NULL,
-  filename VARCHAR(64) NOT NULL,
-  password VARCHAR(256) NOT NULL,
-  created DATETIME default current_timestamp,
-  PRIMARY KEY (username)
+  portfolio_id INT NOT NULL,
+  portfolio_name VARCHAR(20) NOT NULL,
+  created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY(owner) REFERENCES users(username) ON DELETE CASCADE,
+  FOREIGN KEY(portfolio) REFERENCES portfolio(portfolio_id) ON DELETE CASCADE,
+  PRIMARY KEY(optimize_id)
 );
 
+CREATE TABLE portfolio (
+  portfolio_id INT NOT NULL AUTO_INCREMENT,
+  investments VARCHAR(2048) NOT NULL,
+  FOREIGN KEY(owner) REFERENCES users(username) ON DELETE CASCADE,
+  PRIMARY KEY(portfolio_id)
+)
