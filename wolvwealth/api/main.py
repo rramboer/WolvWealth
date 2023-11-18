79ed0d08677cccd6ddc6ec2327f9d6e70dbb00b3
rramboer <ryanramboer@gmail.com>
2023-11-18 15:08:00 -0500

    Create prices.json

--- wolvwealth/api/main.py
+++ wolvwealth/api/main.py
@@ -2,11 +2,37 @@
 
 import flask
 # import pathlib
-# import pypfopt
+import pypfopt
 import wolvwealth
+import yfinance
+import json
 
 
 @wolvwealth.app.route("/api/")
 def api_default():
     """WolvWealth API Usage Endpoint."""
     return flask.jsonify({"/api/": "API Info"})
+
+@wolvwealth.app.route("/price/")
+def retrieve_price():
+    """Return current stock price of ticker"""
+    sp100_tickers = [
+        "AAPL", "ABBV", "ABT", "ACN", "ADBE", "AIG", "ALL", "AMGN", "AMT", "AMZN",
+        "AXP", "BA", "BAC", "BIIB", "BK", "BKNG", "BLK", "BMY", "C", "CAT",
+        "CHTR", "CL", "CMCSA", "COF", "COP", "COST", "CRM", "CSCO", "CVS", "CVX",
+        "DD", "DHR", "DIS", "DOW", "DUK", "EMR", "EXC", "F", "META", "FDX", "GD",
+        "GE", "GILD", "GM", "GOOGL", "GS", "HD", "HON", "IBM", "INTC", "INTU", "ISRG",
+        "JNJ", "JPM", "KHC", "KMI", "KO", "LLY", "LMT", "LOW", "MA", "MCD", "MDLZ",
+        "MDT", "MET", "MMM", "MO", "MRK", "MS", "MSFT", "NEE", "NFLX", "NKE", "NVDA",
+        "ORCL", "OXY", "PEP", "PFE", "PG", "PM", "PYPL", "QCOM", "RTX", "SBUX", "SLB",
+        "SO", "SPG", "T", "TGT", "TMO", "TMUS", "TSLA", "TXN", "UNH", "UNP", "UPS",
+        "USB", "V", "VZ", "WBA", "WFC", "WM", "WMT", "XOM",
+    ]
+    price_dict = {}
+    for ticker in sp100_tickers:
+        price_dict[ticker] = round(yfinance.Ticker(ticker).history(period="1d")["Close"].iloc[-1], 2)
+        print(ticker + " price loaded.")
+    with open("prices.json", "w") as json_file:
+        json.dump(price_dict, json_file, indent=4)
+    print("PRICES JSON UPDATE SUCCESSFUL.")
+    
