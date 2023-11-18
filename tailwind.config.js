21b785225fffa918949fdaffd599430af05d57af
GenericallyNamed <amshand@umich.edu>
2023-11-18 14:49:25 -0500

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./wolvwealth/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- wolvwealth/js/index.jsx
+++ wolvwealth/js/index.jsx
@@ -2,9 +2,20 @@ import React from "react";
 
 function Index() {
     return (
-        <div>
-            Your WolvWealth
-        </div>
+        <>
+            <div id="sideBar">
+                <ul>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Products</li>
+                    <li>Contact</li>
+                    <li>Help</li>
+                </ul>
+            </div>
+            <div id="content" className="flex">
+                Test
+            </div>
+        </>
     )
 }
 
