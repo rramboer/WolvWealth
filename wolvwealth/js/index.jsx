9a8a9801cb266108be4932e3e4dbbc2cc3c83d2f
rramboer <ryanramboer@gmail.com>
2023-11-19 10:59:55 -0500

    Fix Logout on Dashboard

--- wolvwealth/js/index.jsx
+++ wolvwealth/js/index.jsx
@@ -40,7 +40,7 @@ function Index() {
                                 <button value={1} onClick={handleSidebarClick} className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</button>
                             </li>
                             <li>
-                                <a href="/logout/" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</a>
+                                <a href="/accounts/logout/" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</a>
                             </li>
                             </ul>
                         </div>
