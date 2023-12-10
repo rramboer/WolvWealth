0edda24820e8402418e8a7699136cc5b42166471
rramboer <ryanramboer@gmail.com>
2023-12-10 15:00:42 -0500

    Add API key generator

--- wolvwealth/api/__init__.py
+++ wolvwealth/api/__init__.py
@@ -5,7 +5,7 @@ from wolvwealth.api.state import ApplicationState
 import wolvwealth.api.optimize
 import wolvwealth.api.main
 import wolvwealth.api.api_exceptions
+import wolvwealth.api.auth
 import wolvwealth.model
-
 
 state = ApplicationState()  # Preload global resources on startup
