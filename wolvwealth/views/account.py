b912b51ad1e81976e44279138b6ffeb251312671
Cole VanOphem <nvano@umich.edu>
2024-01-18 00:03:29 -0500

    Add accounts page prototype with Flask route Update .gitignore

--- wolvwealth/views/account.py
+++ wolvwealth/views/account.py
@@ -1,5 +1,6 @@
 """Routes for the account page."""
 
+import arrow
 import flask
 from flask import render_template
 import wolvwealth
@@ -11,5 +12,26 @@ def show_account():
     """Display /account route."""
     if not is_logged_in():
         flask.redirect(flask.redirect("show_login"))
-    context = {}
+
+    connection = wolvwealth.model.get_db()
+    cur = connection.execute(
+        "SELECT username, email, created FROM users WHERE username = ?",
+        (flask.session["username"],),
+    )
+    account = cur.fetchone()
+
+    cur = connection.execute(
+        'SELECT uses, expires FROM tokens WHERE owner = ?', (
+            flask.session["username"],)
+    )
+    tokens = cur.fetchone()
+
+    context = {
+        "username": account["username"],
+        "email": account["email"],
+        "created": arrow.get(account["created"]).humanize(),
+        "expiration_date": arrow.get(tokens["expires"]).humanize(),
+        "uses": tokens["uses"],
+    }
+
     return render_template("account.html", **context)
