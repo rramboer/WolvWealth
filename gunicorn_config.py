7c7445209ac19b56e5be2ff933dd99e25457a0ed
rramboer <ryanramboer@gmail.com>
2024-01-31 01:08:56 -0500

    Change gunicorn port to 8000

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,15 +1,15 @@
 import os
 
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
+workers = int(os.environ.get("GUNICORN_PROCESSES", "1"))
 
-threads = int(os.environ.get('GUNICORN_THREADS', '4'))
+threads = int(os.environ.get("GUNICORN_THREADS", "4"))
 
 # timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:80')
+bind = os.environ.get("GUNICORN_BIND", "0.0.0.0:8000")
 
 
-forwarded_allow_ips = '*'
+forwarded_allow_ips = "*"
 
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+secure_scheme_headers = {"X-Forwarded-Proto": "https"}
